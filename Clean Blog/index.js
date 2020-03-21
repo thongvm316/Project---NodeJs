ef0c24996d6e48363bbf2af198168d03e013a620
thongvm316 <56250318+thongvm316@users.noreply.github.com>
2020-03-21 16:18:46 +0700

    2020031

--- Clean Blog/index.js
+++ Clean Blog/index.js
@@ -1,22 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
+// Model Mongoose
+const Post = require('./models/Post');
 
 const app = new express();
+mongoose.connect('mongodb://localhost/portfolio', { useNewUrlParser: true });
 
 // view engine setup
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+/* -------------------Render--------------------- */
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+app.get('/post/new', (req, res) => {
+    res.render('createpost')
+})
+
+app.post('/posts/store', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+    
+    // Post.create({
+    //     ...req.body,
+    // }, (err, post) => {
+    //     console.log(post);
+    //     res.redirect('/');
+    // }); 
+})
 
 
 
+const port = 4000;
 app.listen(4000, () => {
-    console.log('Port: 3000');
+    console.log(`Port: ${port}`);
     
 })
---
+++ Clean Blog/models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: String,
+    subtitle: String,
+    content: String,
+
+    image: String,
+    createAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+module.exports = Post;
